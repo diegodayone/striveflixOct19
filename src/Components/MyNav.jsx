1e3e23b8b839d44433619f5c995cee916e021fe6
Diego Banovaz <diego@strive.school>
2019-11-25 08:44:56 +0100

    comment

--- src/Components/MyNav.jsx
+++ src/Components/MyNav.jsx
@@ -50,6 +50,9 @@ const MyNav = (props) => {
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
+
+          {/* Since it's a functional component, you have no "this" (reference to the component itself)
+          To access the props you just type props (name given in line 17) */}
           <Input type="text" onChange={(input) => props.changeSearch(input.target.value)} placeholder="Search here!"></Input>
         </Collapse>
       </Navbar>
